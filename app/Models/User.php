d8a9a77dd79f8fac11ff5bcbee8a3e336df12ed5
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-05-25 11:47:30 +0200

    Create User.php

---
+++ app/Models/User.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    protected $fillable = ['email', 'password', 'discord_id', 'is_admin'];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            $model->id = (string) Str::uuid();
+        });
+    }
+
+    protected $hidden = ['password'];
+}
